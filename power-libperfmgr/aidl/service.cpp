38ac1ddc105da67a2c6811325b15405b7295ea31
AsTechpro20 <94213632+AsTechpro20@users.noreply.github.com>
2024-11-18 07:34:32 +0530

    Update service.cpp

--- power-libperfmgr/aidl/service.cpp
+++ power-libperfmgr/aidl/service.cpp
@@ -1,19 +1,3 @@
-/*
- * Copyright (C) 2020 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 #define LOG_TAG "powerhal-libperfmgr"
 
 #include <android-base/logging.h>
@@ -38,8 +22,7 @@ using ::android::perfmgr::HintManager;
 constexpr std::string_view kPowerHalInitProp("vendor.powerhal.init");
 
 int main() {
-    // Parse config but do not start the looper
-    std::shared_ptr<HintManager> hm = HintManager::GetInstance();
+    std::shared_ptr<HintManager> hm(HintManager::GetInstance());
     if (!hm) {
         LOG(FATAL) << "HintManager Init failed";
     }
