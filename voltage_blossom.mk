d2a2f6ed87bc8a5c766ffbb21a827618929ba4d0
ragebreaker <125530737+mlm-games@users.noreply.github.com>
2024-12-26 18:56:38 +0530

    Update and rename lineage_blossom.mk to voltage_blossom.mk

--- voltage_blossom.mk
+++ voltage_blossom.mk
@@ -13,13 +13,13 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/non_ab_device.mk)
 $(call inherit-product, device/xiaomi/blossom/device.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/voltage/config/common_full_phone.mk)
 
 scr_resolution := 720
 TARGET_SUPPORTS_BLUR := true
 TARGET_ENABLE_BLUR := true
 
-PRODUCT_NAME := lineage_blossom
+PRODUCT_NAME := voltage_blossom
 PRODUCT_DEVICE := blossom
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := Redmi
